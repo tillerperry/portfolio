1cf9dcc32b04a348c5a8b09758e2a6c72bffb526
LESLIE KOFI BROBBEY <lesliebrobbey@outlook.com>
2025-02-25 21:00:29 +0000

    initial ommit

---
+++ send_mail.php
@@ -0,0 +1,36 @@
+<?php
+require 'PHPMailerAutoload.php';
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+
+$mail->SMTPDebug  = 1;  
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "some-mail@gmail.com";
+$mail->Password   = "";
+
+$mail->IsHTML(true);
+$mail->AddAddress("recipient-email@domain", "recipient-name");
+$mail->SetFrom("some-mail@gmail.com", "Dorglo Godwin");
+// $mail->AddReplyTo("reply-to-email@domain", "reply-to-name");
+// $mail->AddCC("cc-recipient-email@domain", "cc-recipient-name");
+$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+
+$mail->MsgHTML($content); 
+if(!$mail->Send()) {
+  echo "Error while sending Email.";
+  var_dump($mail);
+} else {
+  echo "Email sent successfully";
+}
+?>
